39d0fcf546f9272e77ccf484480bbaa5d86b860e
Kokina Elena <3400051@gmail.com>
2021-08-13 15:47:23 +0300

    bug fix3

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -1,21 +1,19 @@
 
 // import { data } from 'browserslist';
-import { data } from 'browserslist';
+
 import { fotoArr, albumArr, fetchAlbum, fetchFoto } from '../model/collection';
 import { renderFotos, renderAlbums, getFirstAlbumPhotos } from '../view/view';
-
+import { data } from 'browserslist';
 
 
 export function controller() {
-    
+   
     fetchAlbum();
-    renderAlbums(albumArr);  
-    
+    renderAlbums(albumArr); 
     fetchFoto(1);
-    renderFotos(fotoArr);
-
-    getFirstAlbumPhotos(fotoArr[0]);
-           
+    renderFotos(fotoArr);     
+    getFirstAlbumPhotos(fotoArr[0]);        
 }
+
 
 
--- src/js/view/view.js
+++ src/js/view/view.js
@@ -3,28 +3,28 @@ import '../../css/normalize.css';
 import '../../css/dark.css';
 import { PHOTOS_URL } from '../config';
 import { albumArr } from '../model/collection';
- const ALBUM_ITEM_CLASS = 'album-item';
 
+ const ALBUM_ITEM_CLASS = 'album-item';
  const albumEl = document.querySelector('#albums');
  const fotoEl = document.querySelector('#fotos');
-
  const albumTemplate = document.querySelector('#albumTemplate').innerHTML;
- const fotoTemplate = document.querySelector('#fotoTemplate').innerHTML;
+ const fotoTemplate = document.querySelector('#fotoTemplate').innerHTML;   
 
-albumEl.addEventListener('click', onAlbumsClick);
+ albumEl.addEventListener('click', onAlbumsClick);
 
  function onAlbumsClick(e) {
     if (e.target.classList.contains(ALBUM_ITEM_CLASS)) {
         fetchFoto(e.target.dataset.id);
-    }
-}   
-
+   }
+   
+}
 export function renderFotos(data) {
   fotoEl.innerHTML = data.map((foto) => getFotoHTML(foto))
     .join('\n');
 }  
   
- function getFotoHTML(foto) {
+ 
+function getFotoHTML(foto) {
     return fotoTemplate
       .replace('{{url}}', foto.thumbnailUrl)
       .replace('{{title}}', foto.title);
